7750bd636144c2bcbdd7ef6856b5f8f37540451d
youseop <edlsw@naver.com>
2022-11-13 00:23:04 +0900

    connect mongodb

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("youseop");
+// });
+
+// app.get("/objects", (req, res) => {
+//   res.send("objects");
+// });
+
+const connectDB = require("./config/db");
+
+//Load Config
+dotenv.config({ path: "./config/config.env" });
+connectDB();
 
 app.listen(8080);
